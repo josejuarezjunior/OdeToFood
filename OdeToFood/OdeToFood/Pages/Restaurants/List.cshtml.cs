213646b95d6e05e43085f864c6a5765ef39260ea
Junior <Josejuarez.Junior@smartm.com>
2022-10-07 21:49:43 -0300

    Working with Models and Model Binding

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,15 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
         public string Message { get; set; }
-        public void OnGet()
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet(string searchTerm)
         {
             Message = "Hello, World!";
+            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
+
         }
     }
 }
