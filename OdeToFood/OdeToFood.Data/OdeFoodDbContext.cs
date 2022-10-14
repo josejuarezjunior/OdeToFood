d330912b9c1b4f68476d39d622bf1a3d8c586fee
Junior <Josejuarez.Junior@smartm.com>
2022-10-14 20:51:44 -0300

    Implementig create restaurant and configuring Entity Framework Core

---
+++ OdeToFood/OdeToFood.Data/OdeFoodDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public class OdeFoodDbContext : DbContext
+    {
+        public OdeFoodDbContext(DbContextOptions<OdeFoodDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
